7dc83c34e6e88d534b06097cc14ca3347d242cb3
fuchunhui <fuchuhu7@gmail.com>
2018-11-29 21:53:03 +0800

    add less style

--- src/components/nonet/NonetTable.vue
+++ src/components/nonet/NonetTable.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="nonet">
-        
+        <p>这是一段测试文字</p>
     </div>
 </template>
 
@@ -25,9 +25,14 @@ export default {
 </script>
 
 <style lang="less">
+@import url('../../assets/css/variables');
 .nonet {
     width: 100%;
     height: 400px;
     background-color: #f2f2f2;
+
+    p {
+        color: @nonet-test;
+    }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import './assets/css/base.less';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
